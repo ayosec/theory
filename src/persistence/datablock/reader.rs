6f14b8da5a83862a233fe95cb4b19764eb287196
Ayose <ayosec@gmail.com>
2022-10-06 23:34:47 +0100

    Use a LRU cache for DataBlocksReader
    It is expected that some data blocks (like the one with page metadata) is
    accessed multiple times. A LRU cache can help to reduce the read operations on
    the input stream.
    
    The cache stores the full and uncompressed block.

--- src/persistence/datablock/reader.rs
+++ src/persistence/datablock/reader.rs
@@ -1,19 +1,35 @@
 //! Reader for data blocks.
 
+use core::num::NonZeroUsize;
 use std::io::{self, Read, Seek, SeekFrom};
 
 use super::BlockType;
+
+/// Size of the LRU cache.
+const LRU_CACHE_SIZE: NonZeroUsize = match NonZeroUsize::new(16) {
+    // TODO use Option::unwrap when const_option feature is stable.
+    Some(n) => n,
+    None => panic!(),
+};
 
 pub(crate) struct DataBlocksReader<S> {
     stream: S,
 
     stream_len: u64,
+
+    cache: lru::LruCache<u64, Result<Vec<u8>, io::Error>>,
 }
 
 impl<S: Read + Seek> DataBlocksReader<S> {
     pub(crate) fn new(mut stream: S) -> io::Result<Self> {
         let stream_len = stream.seek(SeekFrom::End(0))?;
-        Ok(DataBlocksReader { stream, stream_len })
+        let cache = lru::LruCache::new(LRU_CACHE_SIZE);
+
+        Ok(DataBlocksReader {
+            stream,
+            stream_len,
+            cache,
+        })
     }
 
     /// Return a mutable reference to the input stream.
@@ -42,47 +58,56 @@ impl<S: Read + Seek> DataBlocksReader<S> {
             )
         })?;
 
-        // TODO use LRU cache
-        self.stream.seek(SeekFrom::Start(block_id))?;
+        let result = self.cache.get_or_insert(block_id, || {
+            self.stream.seek(SeekFrom::Start(block_id))?;
 
-        // Block type.
-        let mut byte_tag = [0];
-        self.stream.read_exact(&mut byte_tag)?;
+            // Block type.
+            let mut byte_tag = [0];
+            self.stream.read_exact(&mut byte_tag)?;
 
-        let block_type = BlockType::try_from(byte_tag[0])
-            .map_err(|_| io::Error::new(io::ErrorKind::Other, "Invalid block type"))?;
+            let block_type = BlockType::try_from(byte_tag[0])
+                .map_err(|_| io::Error::new(io::ErrorKind::Other, "Invalid block type"))?;
 
-        // Block length.
-        //
-        // Return an error if the length is beyond the end of the input.
-        let mut len = [0; 4];
-        self.stream.read_exact(&mut len)?;
-        let len = u32::from_be_bytes(len);
+            // Block length.
+            //
+            // Return an error if the length is beyond the end of the input.
+            let mut len = [0; 4];
+            self.stream.read_exact(&mut len)?;
+            let len = u32::from_be_bytes(len);
 
-        if offset > len as usize {
-            return Err(io::Error::new(
-                io::ErrorKind::InvalidInput,
-                "offset is beyond end of the block",
-            ));
-        }
-
-        if block_id.saturating_add(len as u64) > self.stream_len {
-            return Err(io::Error::new(
-                io::ErrorKind::InvalidData,
-                "Block beyond the end of the input",
-            ));
-        }
-
-        // Block data.
-        let mut data;
-
-        match block_type {
-            BlockType::Uncompressed => {
-                data = vec![0; len as usize];
-                self.stream.read_exact(&mut data)?;
+            if offset > len as usize {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "offset is beyond end of the block",
+                ));
             }
-        }
 
-        Ok(f(&data[offset..]))
+            if block_id.saturating_add(len as u64) > self.stream_len {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    "Block beyond the end of the input",
+                ));
+            }
+
+            // Block data.
+            let mut data;
+
+            match block_type {
+                BlockType::Uncompressed => {
+                    data = vec![0; len as usize];
+                    self.stream.read_exact(&mut data)?;
+                }
+            }
+
+            Ok(data)
+        });
+
+        result
+            .as_ref()
+            .map(|data| f(&data[offset..]))
+            .map_err(|e| match e.get_ref() {
+                Some(r) => io::Error::new(e.kind(), r.to_string()),
+                None => io::Error::new(e.kind(), ""),
+            })
     }
 }
